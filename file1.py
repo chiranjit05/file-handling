326e3a73b6e29c54cdee044ca404da56caa32502
chiranjit05 <46133445+chiranjit05@users.noreply.github.com>
2019-12-26 10:23:04 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,3 +1,4 @@
+import os
 
 print("Enter 1 to add data.")
 print("Enter 2 to update .")
@@ -7,14 +8,20 @@ print("Enter 5 to search data.")
 
 inp=int(input("your input is : "))
 
-
+def extra():
+    if(inp==1):
+        add()
+    if(inp==2):
+        update()
     
 def add():
-    n=int(input("how many entries do u want to make : "))
-    for i in range(n):
-        roll=input("Enter the roll no : ")
-        name=input("Enter he name : ")
-        add=input("Enter the address : ")
+        n=int(input("how many entries do u want to make : "))
+        for i in range(n):
+            roll=input("Enter the roll no : ")
+            name=input("Enter he name : ")
+
+            add=input("Enter the address : ")
+
 
 
         f1=open("file1.log","a") #opening the file
@@ -26,8 +33,8 @@ def add():
         f1.write(";")
         f1.write(add)
         f1.write(".")
-   
-    f1.close()
+        f1.close
+        
 
  
 
@@ -56,20 +63,42 @@ def search():
                         print("Number has been found")
                         flag=1
                         break
+def update():
+    f1=open("file1.log",'r')
+    f2=open("file2.log",'w+')
+    search=input("Enter the element to be searced : ")
+    
+    for line in f1:
+        string = line.split(".")
+        for line in string:
+            string=line.split(";")
+            if(string[0]==search):
+                    name=input("Enter name : ")
+                    address=input("enter adress:")
+                    f2.write(search + ";")
+                    f2.write(name+";")
+                    f2.write(address+";")
+            else:
+                f2.write(line)
+    f1.close()
+    f2.close()
+    os.remove("file1.log")
+    os.rename(r'C:\Users\admin\Desktop\chir\file2.log',r'C:\Users\admin\Desktop\chir\file1.log')
+        
+                        
 
-    if (flag==0):
-            print("number not found")
-    else:
-            print("found")
+
+
 
             
 
-    
-if(inp == 1):
-    add()
-if(inp==2):
+    if(inp == 1):
+        add()
+if(inp==4):
     #print("this part of the code has ben reached")
     showall()
 if(inp==5):
-    search()
+        search()
+if(inp==2):
+        update()
     
